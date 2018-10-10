fd52c7652e4efb96442df87fda969c59aee65e33
quakex <iquakex@gmail.com>
2018-10-10 16:41:10 +0800

    page navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'index', to: 'pages#index', as: 'index'
+  get 'about', to: 'pages#about', as: 'about'
+  get 'contact', to: 'pages#contact', as: 'contact'
+  root 'pages#index'
 end
